efc7e68f2450ecd12af301321659db29372cfd03
Kha Tran <khabaka321998@gmail.com>
2019-07-21 13:03:34 +0700

    heroku config v2

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
+// server.js
 const express = require('express');
-const path = require('path');
 const app = express();
-
-// Serve static files....
-app.use(express.static(__dirname + '/dist/MY_APP_NAME_HERE'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME_HERE/index.html'));
-});
-
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
